ffad06ed653629da3ab628d6be57bd5513dc3ddc
IanMuriithi <ian.muriithi1@student.moringaschool.com>
2025-03-13 12:58:30 +0300

    Modifies index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";
+const height = Math.min(39, Math.max(1, 40)); // Ensures height stays between 1 and 39
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
